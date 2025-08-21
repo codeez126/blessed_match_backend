f24a14d02b534cd668b7f87e22f1294a7cc223c0
Muhammad Furqan Aslam <88925039+Furqan1122@users.noreply.github.com>
2025-08-21 12:44:08 +0500

    ,

--- source/routes/web.php
+++ source/routes/web.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
+Route::get('/', function () {
+    return Auth::guard('admin')->check() ? redirect()->route('admin.dashboard') : redirect()->route('adminlogin');
+});
 /** Super Admin and admins routes **/
 Route::get('admin/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'adminloginform'])
     ->name('adminlogin');
