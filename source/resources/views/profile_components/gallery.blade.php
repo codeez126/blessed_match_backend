65a0dfeeb3b3fc378fb215029b56cc002b2c768f
Muhammad Furqan Aslam <88925039+Furqan1122@users.noreply.github.com>
2025-09-02 11:16:06 +0500

    Update gallery.blade.php

--- source/resources/views/profile_components/gallery.blade.php
+++ source/resources/views/profile_components/gallery.blade.php
@@ -2,7 +2,7 @@
 
 <div class="row g-1">
     @foreach($user->clientImages as $images)
-    <div class="col-md-3 col-6 theme-card-points">
+    <div class="col-md-3 col-6 mb-2">
         <div class="advanced-info-section d-flex align-items-center">
             <img src="{{asset($images->image)}}" alt="Profile Image" class="img-fluid">
         </div>
