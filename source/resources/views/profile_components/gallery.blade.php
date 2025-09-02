0559e64ee73499838a3cdf753f19ef59392c0e8c
Muhammad Furqan Aslam <88925039+Furqan1122@users.noreply.github.com>
2025-09-02 11:10:44 +0500

    m

--- source/resources/views/profile_components/gallery.blade.php
+++ source/resources/views/profile_components/gallery.blade.php
@@ -1,6 +1,6 @@
 <h4 class="repeating-heading">Gallery</h4>
 
-<div class="row">
+<div class="row g-1">
     @foreach($user->clientImages as $images)
     <div class="col-md-3 col-6 theme-card-points">
         <div class="advanced-info-section d-flex align-items-center">
